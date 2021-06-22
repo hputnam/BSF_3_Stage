605ad5a38d28aa9c72b94499349ca51588a257bf
Erin Chille <erin_chille@my.uri.edu>
2021-06-22 11:50:47 -0400

    adding all vst-transformed gene count matrices

--- Scripts/4-assembly-transcript-quantification-stringtie.sh
+++ Scripts/4-assembly-transcript-quantification-stringtie.sh
@@ -3,11 +3,11 @@
 #SBATCH -t 100:00:00
 #SBATCH --export=/opt/software/StringTie/2.1.4-GCC-9.3.0/bin/prepDE.py
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=erin_chille@uri.edu
+#SBATCH --mail-user=XXX
 #SBATCH --exclusive
 #SBATCH --nodes 1 --ntasks-per-node=20
 #SBATCH --mem=500GB
-#SBATCH -D /data/putnamlab/erin_chille/BSF_3_Stage/Spis/hisat2_bam
+#SBATCH -D Spis/hisat2_bam
 
 ##This script is to assemble and quantify gene counts from the RNA sequences of three life stages of three coral species to respective genome assemblies
 #https://github.com/hputnam/BSF_3_Stage
@@ -17,27 +17,36 @@
 # M. capitata ENA Project (TBD; Link)
 
 echo "Loading programs" $(date)
-module load StringTie/2.1.4-GCC-9.3.0 #Transcript assembly: StringTie
-module load gffcompare/0.11.5-foss-2018b #Transcript assembly QC: GFFCompare
+module load StringTie/2.1.3-GCC-8.3.0 #Transcript assembly: StringTie
+module load GffCompare/0.12.1-GCCcore-8.3.0 #Transcript assembly QC: GFFCompare
 module list
 
 #StringTie reference-guided assembly
 
 echo "Starting assembly!" $(date)
 
-array3=($(ls *.bam)) #Make an array of sequences to assemble
-for i in ${array3[@]}; do #Running with the -e option to compare output to exclude novel genes. Also output a file with the gene abundances
-        stringtie -p $SLURM_NTASKS_PER_NODE -e -G ../ref/Spis.genome.annotation.gff3 -o ../stringtie_out_files/${i}.gtf ${i}
-        echo "StringTie-assembly-to-ref ${i}" $(date)
+array1=($(ls *.bam)) #Make an array of sequences to assemble
+for i in ${array1[@]}; do 
+	stringtie -p $SLURM_NTASKS_PER_NODE -e --conservative -C ../stringtie_out_files/$(echo ${i}|sed s/.fastq.gz.bam/_coverage/) -G ../ref/Spis.genome.annotation.gff3 -o ../stringtie_out_files/${i}.gtf ${i}
+    grep -c "transcript" ../stringtie_out_files/$(echo ${i}|sed s/.fastq.gz.bam/_coverage.gtf/)
+	zgrep -c "exon" ../stringtie_out_files/$(echo ${i}|sed s/.fastq.gz.bam/_coverage.gtf/)
+	echo "StringTie-assembly-to-ref ${i}" $(date)
 done
-echo "Assembly complete! Starting assembly analysis..." $(date)
+#Running with the -e option to compare output to exclude novel genes. Also outputs GTF file for each assembly containing only sequences with full coverage
+#Out file prints the number of fully covered transcripts and exons, respectively 
 
-cd /data/putnamlab/erin_chille/BSF_3_Stage/Spis/stringtie_out_files #change working dir
+echo "Assembly complete! Starting assembly evaluation." $(date)
 
-stringtie --merge -p $SLURM_NTASKS_PER_NODE -G ../ref/Spis.genome.annotation.gff3 -o Spis_merged.gtf ../stringtie_mergelist.txt #Merge GTFs to form full GTF for analysis of assembly accuracy and precision
-echo "Stringtie merge complete" $(date)
+cd Spis/stringtie_out_files #change working dir
 
-gffcompare -r ../ref/Spis.genome.annotation.gff3 -G -o Spris_merged Spis_merged.gtf #Compute the accuracy and precision of assembly
-echo "GFFcompare complete! Starting gene count matrix assembly..." $(date)
+gffcompare -r ../ref/Spis.genome.annotation.gff3 -R -G -V -o Spis -i ../stringtie_mergelist.txt #Compute the accuracy and precision of assembly
+	
+        # -r = Reference
+        # -R = Ignore reference sequences with no aligned transcripts in any 1 sample to calculate sensitivity
+        # -Q = Ignore reference sequences with no aligned transcripts in all samples to calculate sensitivity
+        # -V = Verbose mode
+        # -o = output prefix is sample ID
+        # -i = text file with query file paths
 
+echo "Assessment of GTFs complete! Starting gene count matrix assembly." $(date)
 python /opt/software/StringTie/2.1.4-GCC-9.3.0/bin/prepDE.py -g Spis_gene_count_matrix.csv -i ../stringtie_samplelist.txt #Compile the gene count matrix
